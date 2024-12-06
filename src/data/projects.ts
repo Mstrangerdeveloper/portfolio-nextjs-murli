b1a76bd160c2c59a482d398352b842cf2c68135c
Mstrangerdeveloper <murli274301@gmail.com>
2024-12-06 11:57:17 +0530

    project description

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -15,27 +15,63 @@ const projects: IProjectItem[] = [
   // },
   {
     id: "e-commerce-app-mern",
-    title: "E-commerce App",
+    title: "E-commerce web App(Exsale)",
     description:
-      "An e-commerce web application developed using React.js, Material UI, Redux, and Stripe.",
+      "A React.js e-commerce platform enabling users to sell, buy, or list free products. Built with Redux for seamless state management, Tailwind CSS for modern styling, and Stripe for secure payments. Includes community sections to connect locally, fostering engagement and easy trading within specific user communities.",
     icon: "/skills/react.svg",
     repoType: RepoType.Public,
     projectType: ProjectType.Personal,
-    githubUrl: "https://github.com/Mstrangerdeveloper/E-COM_SHOP",
-    url: "https://github.com/Mstrangerdeveloper/E-COM_SHOP",
+    // githubUrl: "https://github.com/Mstrangerdeveloper/E-COM_SHOP",
+    // url: "https://github.com/Mstrangerdeveloper/E-COM_SHOP",
     tags: ["React.js", "Redux", "Material UI", "Stripe"],
+  },
+  {
+    id: "e-commerce-app-mern",
+    title: "Cloud-Based Management System",
+    description:
+      "Created a scalable cloud-based management system to streamline inventory management and client onboarding for businesses and enterprises,resulting in a redesigned UI thatreduced user complaints by 40% and increased satisfaction ratings by 20%.",
+    icon: "/skills/react.svg",
+    repoType: RepoType.Public,
+    projectType: ProjectType.Personal,
+    // githubUrl: "https://github.com/Mstrangerdeveloper/E-COM_SHOP",
+    // url: "https://github.com/Mstrangerdeveloper/E-COM_SHOP",
+    tags: ["React.js", "Redux", "Bootstrap",],
+  },
+  {
+    id: "fayredge",
+    title: "Online Review and Survey Platform",
+    description:
+      "Developed an online platform for company reviews and stakeholder surveys, achieving 85% user satisfaction in the first month. Streamlined feedback processes,reducing survey design timeby 50%, and enabling a focus on data analysis and strategy",
+    icon: "/skills/react.svg",
+    repoType: RepoType.Public,
+    projectType: ProjectType.Personal,
+    githubUrl: "https://github.com/Mstrangerdeveloper/FayrEdge_Next_js",
+    url: "https://review.fayredge.com/",
+    tags: ["React.js", "Redux", "Material UI", "Nextjs"],
   },
    {
     id: "solar-app-mern",
     title: "Solar web App",
     description:
-      "An solar web application developed using React.js,Chakra  UI, Redux,.",
+      "Implemented a solar management system for companies to handle leads for solar panel installations, featuring a  redesigned UI that led to a 40% reduction in complaints and a 20% increase in satisfaction ratings",
     icon: "/skills/react.svg",
     repoType: RepoType.Public,
     projectType: ProjectType.Personal,
     githubUrl: "https://github.com/Mstrangerdeveloper/solar/tree/main/frontend",
     url: "https://github.com/Mstrangerdeveloper/solar/tree/main/frontend",
-    tags: ["React.js", "Redux", "Chakra  UI"],
+    tags: ["React.js", "Redux", "Tailwind CSS"],
+  },
+  {
+    id: "solar-app-mern",
+    title: "Vehicle Scrap Request Management System(TVS)",
+    description:
+      "Developed a Vehicle Scrap Request Management System for efficient handling of vehicle end-of-life processes. Built with React.js, TypeScript, and Tailwind CSS, the system ensures a seamless user experience with responsive design and robust state management using Redux. It streamlines scrap requests, improves workflow efficiency, and integrates modern UI for real-time updates and management",
+    icon: "/skills/react.svg",
+    repoType: RepoType.Public,
+    projectType: ProjectType.Personal,
+    githubUrl: "https://github.com/Mstrangerdeveloper/TVS_React_js",
+    url: "https://github.com/Mstrangerdeveloper/TVS_React_js",
+    tags: ["React.js", "Redux", "Tailwind CSS"],
   },
  
   {
@@ -46,7 +82,7 @@ const projects: IProjectItem[] = [
     icon: "/skills/nestjs.svg",
     repoType: RepoType.Public,
     projectType: ProjectType.Personal,
-    githubUrl: "https://github.com/nixrajput/social-media-api-nodejs",
+    // githubUrl: "https://github.com/nixrajput/social-media-api-nodejs",
     tags: ["Node.js", "Express.js", "MongoDB", "WebSocket"],
   },
  
@@ -64,16 +100,16 @@ const projects: IProjectItem[] = [
 
 
   {
-    id: "portfolio-website",
-    title: "Portfolio Website",
+    id: "interview-Portal",
+    title: "Interview Portal",
     description:
-      "This repository contains the source code for a portfolio website built using Next.js and Sass.",
+      "Designed an interview portal that enables companies to schedule and conduct online interviews with candidates, improving the hiring process. Achieved a 90% user satisfaction rate based on feedback from companies and candidates Video and audio recording: Capabilities for comprehensive interview sessions to schedule Online code editor and compiler: forreal-time coding assessments. Dynamic question and position-based coding questions to evaluate candidates’ skills effectively.",
     icon: "/skills/nextjs.png",
     repoType: RepoType.Public,
     projectType: ProjectType.Personal,
     // githubUrl: "https://github.com/nixrajput/portfolio-website",
     // url: "https://nixrajput.dev",
-    tags: ["Next.js", "Sass", "Web Development"],
+    tags: ["Next.js", "Redux js", "Web Development","Material-UI"],
   },
 
 ];
